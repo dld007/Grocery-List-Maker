ee1e417e24201d23839c42591163441f038c523d
dld007 <48293479+dld007@users.noreply.github.com>
2021-01-05 16:05:37 -0500

    Update grocery.py
    Added save to .txt file feature.

--- grocery.py
+++ grocery.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 import json
 import fractions
 from decimal import Decimal
+import keyboard
+
 
 def s_get(url):
     # Attempts to get content from url by making an HTTP GET request.
@@ -19,34 +21,38 @@ def s_get(url):
                 return None
 
     except RequestException as e:
-        log_error('Error during requests to {0} : {1}'.formal(url,str(e)))
+        log_error('Error during requests to {0} : {1}'.formal(url, str(e)))
         return None
 
+
 def is_good_response(resp):
-    #Returns True if response seems to be HTML, False, Otherwise
+    # Returns True if response seems to be HTML, False, Otherwise
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code == 200
             and content_type is not None
             and content_type.find('html') > -1)
 
+
 def log_error(e):
-    #Prints error to console
+    # Prints error to console
     print(e)
 
+
 def get_ingredients(url):
-    #Downloads the recipe page and returns a list of strings, one per ingredient
+    # Downloads the recipe page and returns a list of strings, one per ingredient
     response = s_get(url)
 
     if response is not None:
         soup = BeautifulSoup(response, 'html.parser')
-        data = soup.find('script', type='application/ld+json');
+        data = soup.find('script', type='application/ld+json')
         oJson = json.loads(data.contents[0])
-        #print(oJson['@graph'][7]["recipeIngredient"])
+        # print(oJson['@graph'][7]["recipeIngredient"])
         ingr = oJson['@graph'][7]["recipeIngredient"]
         return ingr
 
+
 def get_grocery_list():
-    measurements = {'Tbsp', 'Tsp', '{}oz','cup', 'cups', 'tsp', 'tbsp'}
+    measurements = {'Tbsp', 'Tsp', '{}oz', 'cup', 'cups', 'tsp', 'tbsp'}
     grocery_list = {}
     print("Enter recipe URLs. Press ENTER when done.")
     inputs = []
@@ -58,7 +64,7 @@ def get_grocery_list():
         for ingr in newList:
             ingrtemp = set(ingr.split())
 
-            #Check if there is a measurement in the ingredient name
+            # Check if there is a measurement in the ingredient name
             measurementType = ""
             if not ingr[0].isdigit():
                 ingrNameAct = ingr.rsplit(' ', 1)[0].strip()
@@ -72,11 +78,12 @@ def get_grocery_list():
                     ingrNameAct = ingr.split(" ", 1)[1].rsplit(' ', 1)[0].strip()
                     measurementAct = ingr.split(" ", 1)[0].strip()
 
-            #Check if the ingredient is already in grocery_list
+            # Check if the ingredient is already in grocery_list
             if ingrNameAct in grocery_list:
-                #If their measurement types match, add their measurements together
-                if (len(measurementType) > 0 and set(grocery_list[ingrNameAct]) & set(measurementType)):
-                    existingMeasurementAmt = convert_to_float(grocery_list[ingrNameAct].split(measurementType)[0].strip())
+                # If their measurement types match, add their measurements together
+                if (len(measurementType) > 0 and (set(grocery_list[ingrNameAct]) & set(measurementType))):
+                    existingMeasurementAmt = convert_to_float(
+                        grocery_list[ingrNameAct].split(measurementType)[0].strip())
                     thisMeasurementAmt = convert_to_float(ingr.split(measurementType)[0].strip())
                     newMeasurementAmt = existingMeasurementAmt + thisMeasurementAmt
                     if not newMeasurementAmt % 1 == 0:
@@ -89,7 +96,7 @@ def get_grocery_list():
                 elif len(measurementType) > 0:
                     grocery_list[ingrNameAct] = grocery_list[ingrNameAct] + " and " + measurementAct
 
-                #No measurement type
+                # No measurement type
                 elif not measurementAct == "":
                     existingMeasurementAmt = convert_to_float(grocery_list[ingrNameAct])
                     thisMeasurementAmt = convert_to_float(measurementAct)
@@ -103,30 +110,49 @@ def get_grocery_list():
                 else:
                     grocery_list[ingrNameAct] = ""
 
-            #Ingredient is not in grocery list
+            # Ingredient is not in grocery list
             else:
                 grocery_list[ingrNameAct] = measurementAct
 
+    full_groc_list = []
     for ingr in grocery_list:
-        print (grocery_list[ingr] + " " + ingr)
+        print(grocery_list[ingr] + " " + ingr)
+        full_groc_list.append(grocery_list[ingr] + " " + ingr)
+
+    return full_groc_list
+
 
 def main():
-    get_grocery_list()
-    #get_ingredients('https://www.budgetbytes.com/black-bean-burgers/')
+    groceryList = get_grocery_list()
+    print("Press S to save list, or press any other key to exit")
+    key = keyboard.read_key()
+    if key == 's':
+        save_to_file(groceryList)
+        exit(0)
+    else:
+        exit(0)
+
 
 def convert_to_float(frac_str):
     try:
         return float(frac_str)
     except ValueError:
         num, denom = frac_str.split('/')
-        try:
-            leading, num = num.split(' ')
-            whole = float(leading)
-        except ValueError:
-            whole = 0
+    try:
+        leading, num = num.split(' ')
+        whole = float(leading)
+    except ValueError:
+        whole = 0
         frac = float(num) / float(denom)
-        return whole - frac if whole < 0 else whole + frac
+    return whole - frac if whole < 0 else whole + frac
+
+
+def save_to_file(groc_list):
+    print("\nInput text file name")
+    file_name = input()
+    with open("%s.txt" % file_name, 'w') as filehandle:
+        filehandle.writelines("%s\n" % ingr for ingr in groc_list)
+
 
 if __name__ == "__main__":
     main()
-
